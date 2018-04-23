87b024ace731af5d60edea14c1cf2691a38e48f3
Andrew Wood <awood7@nd.edu>
2018-04-23 15:37:18 -0400

    code to run gem5....gem5 outputs when it finishes

--- gem5_src/main.cc
+++ gem5_src/main.cc
@@ -1,80 +1,111 @@
 // SYSTEM IMPORTS
 #include <pocketsphinx.h>
-#include <string>
-#include <vector>
 
 
-// C++ PROJECT IMPORTS
+// C PROJECT IMPORTS
 
-
-std::string get_dir_of_file() {
-    std::string current_path = __FILE__;
-    size_t found = current_path.find_last_of("/\\');
-    return current_path.substr(0, found);
-}
-
-std::string path_join(std::vector<std::string>& args)
+int predict(int current_filename, ps_decoder_t* ps)
 {
-    std::string joined_path;
-    for(std::string& arg: args)
-    {
-        joined_path += "/";
-        joined_path += arg;
-    }
-    return joined_path;
-}
-
-int main(int argc, char** argv)
-{
-    current_dir = get_dir_of_file();
-
-    ps_decoder_t *ps;
-    cmd_ln_t *config;
-    FILE *fh;
+    FILE* fh;
+    int rv;
     char const *hyp, *uttid;
     int16 buf[512];
-    int rv;
     int32 score;
 
-    config = cmd_ln_init(NULL, ps_args(), TRUE,
-		         "-hmm", MODELDIR "/en-us/en-us",
-		         "-lm", MODELDIR "/en-us/en-us.lm.bin",
-	    		 "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
-		         NULL);
-    if (config == NULL) {
-	fprintf(stderr, "Failed to create config object, see log for details\n");
-	return -1;
-    }
+    const char* static_file_tmp = "./test_files/%d.wav";
+    char path_buf[30];
+    sprintf(path_buf, static_file_tmp, current_filename);
 
-    ps = ps_init(config);
-    if (ps == NULL) {
-	fprintf(stderr, "Failed to create recognizer, see log for details\n");
-	return -1;
-    }
-
-    fh = fopen("goforward.raw", "rb");
-    if (fh == NULL) {
-	fprintf(stderr, "Unable to open input file goforward.raw\n");
-	return -1;
+    fh = fopen(path_buf, "rb");
+    if(fh == NULL) {
+        fprintf(stderr, "Unable to open input file\n");
+        return -1;
     }
 
     rv = ps_start_utt(ps);
-
     while (!feof(fh)) {
-	size_t nsamp;
-	nsamp = fread(buf, 2, 512, fh);
-	rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
+	    size_t nsamp;
+	    nsamp = fread(buf, 2, 512, fh);
+	    rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
     }
 
     rv = ps_end_utt(ps);
     hyp = ps_get_hyp(ps, &score);
-    printf("Recognized: %s\n", hyp);
-
     fclose(fh);
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    ps_decoder_t *ps;
+    cmd_ln_t *config;
+    int frate;
+    char frate_buff[1000];
+    int max_filename = 75;
+    bool no_error = true;
+
+    if(argc < 2) {
+        fprintf(stderr, "Usage: <program> <window_size>");
+        return -1;
+    }
+    if((frate = atoi(argv[1])) == 0) {
+        fprintf(stderr, "INVALID ARG: frame rate could not be read or was 0.");
+        return -1;
+    }
+    else if(frate > 1000) {
+        fprintf(stderr, "INVALID ARG: frame rate must be between 0 -> 1000");
+        return -1;
+    }
+
+    sprintf(frate_buff, "%d", frate);
+
+    config = cmd_ln_init(NULL, ps_args(), TRUE,
+		         "-hmm", MODELDIR "/cmusphinx-en-us-8khz-5.2",
+		         "-lm", MODELDIR "/en-us/en-us-phone.lm.bin",
+	    		 "-dict", MODELDIR "/en-us/cmudict.dic",
+                 // "-allphone", "phone.lm",
+                 "-feat", "1s_c_d_dd",
+                 "-ceplen", "13",
+                 "-ncep", "13",
+                 "-lw", "10",
+                 "-frate", frate_buff,
+                 "-fwdflatlw", "10",
+                 "-bestpathlw", "10",
+                 "-beam", "1e-80",
+                 "-wbeam", "1e-40",
+                 "-fwdflatbeam", "1e-80",
+                 "-fwdflatwbeam", "1e-40",
+                 "-pbeam", "1e-80",
+                 "-lpbeam", "1e-80",
+                 "-lponlybeam", "1e-80",
+                 "-wip", "0.2",
+                 "-agc", "none",
+                 "-varnorm", "no",
+                 "-cmn", "current",
+                 "-remove_noise", "no",
+                 "-remove_silence", "no",
+                 "-transform", "dct",
+		         NULL);
+    if (config == NULL) {
+	    fprintf(stderr, "Failed to create config object, see log for details\n");
+	    return -1;
+    }
+
+    ps = ps_init(config);
+    if (ps == NULL) {
+	    fprintf(stderr, "Failed to create recognizer, see log for details\n");
+	    return -1;
+    }
+
+    int i = 0;
+    while(i < max_filename && no_error) {
+        no_error = predict(i, ps) == 0;
+        ++i;
+    }
+
     ps_free(ps);
     cmd_ln_free_r(config);
 
     return 0;
-}
 }
 
